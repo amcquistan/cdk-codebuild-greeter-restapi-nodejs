a160eefe0221cbd0d339f78de59817f2c1a24a95
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-01-11 12:18:40 -0600

    fix build

--- src/greeter-api/api.test.js
+++ src/greeter-api/api.test.js
@@ -7,7 +7,7 @@ describe("Test /greet", () => {
   test("Default should return 'Hello World'", async () => {
     const response = await request(app).get("/greet")
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ greeting: 'Hello World - Broken' })
+    expect(response.body).toEqual({ greeting: 'Hello World' })
   });
 
   test("Query param 'Friend' should return 'Hello Friend'", async () => {
