eb4d2c563bba86646fd53ce16c0fc786d8b07ea2
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-01-10 21:18:43 -0600

    added initial buildspec

--- bin/cdk-codebuild-greeter-restapi-nodejs.ts
+++ bin/cdk-codebuild-greeter-restapi-nodejs.ts
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkCodebuildGreeterRestapiNodejsStack } from '../lib/cdk-codebuild-greeter-restapi-nodejs-stack';
+import { CdkCodebuildStack } from "../lib/cdk-codebuild-stack";
+
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+};
 
 const app = new cdk.App();
-new CdkCodebuildGreeterRestapiNodejsStack(app, 'CdkCodebuildGreeterRestapiNodejsStack', {
+
+const codebuildStack = new CdkCodebuildStack(app, 'CdkCodeBuildStackGreeterNodeJsRestApi', {
+  env
+});
+// new CdkCodebuildGreeterRestapiNodejsStack(app, 'CdkCodebuildGreeterRestapiNodejsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +28,4 @@ new CdkCodebuildGreeterRestapiNodejsStack(app, 'CdkCodebuildGreeterRestapiNodejs
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// });
--- lib/cdk-codebuild-stack.ts
+++ lib/cdk-codebuild-stack.ts
@@ -1,15 +1,38 @@
 import * as cdk from "aws-cdk-lib";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class CdkCodebuildStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
-    const gh = codebuild.Source.gitHub({
-      owner: 'amcquistan'
+
+    const artifactBucket = new s3.Bucket(this, 'ArtifactBucket', {
+      version: true
+    });
+
+    const githubSource = codebuild.Source.gitHub({
+      owner: 'amcquistan',
+      repo: 'cdk-codebuild-greeter-restapi-nodejs',
+      webhook: true,
+      webhookFilters: [
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PULL_REQUEST_CREATED)
+          .andBranchIs('main'),
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PULL_REQUEST_UPDATED)
+          .andBranchIs('main'),
+        codebuild.FilterGroup.inEventOf(codebuild.EventAction.PULL_REQUEST_REOPENED)
+          .andBranchIs('main')
+      ]
+    });
+
+    const buildProject = new codebuild.Project(this, 'BuildProject', {
+      source: githubSource,
+      
     })
 
-
+    new cdk.CfnOutput(this, 'ArtifactBucketName', {
+      value: artifactBucket.bucketName
+    });
   }
 }
